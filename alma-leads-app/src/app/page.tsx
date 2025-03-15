41d29290f3ffeb3a1ece6558b62a6d2859fa0c01
Prajakta Pikale <pjprajakta499@gmail.com>
2025-03-14 20:03:30 -0500

    Enhanced welcome pege

--- alma-leads-app/src/app/page.tsx
+++ alma-leads-app/src/app/page.tsx
@@ -1,31 +1,39 @@
 "use client";
 import Link from "next/link";
 import { useAuth } from "@/context/AuthContext";
+import React from "react";
 
 export default function Home() {
     const auth = useAuth();
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1 className="text-2xl font-bold mb-6 text-slate-800">Welcome to Alma!</h1>
-            <div className="space-x-4">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
+            <img
+                src="https://app.ashbyhq.com/api/images/org-theme-wordmark/7d6b6dba-6b28-4a48-b58b-7178236b4ba7/5d8e748d-c0cc-4a0e-b525-94fa977689a4/59d6773a-8a4a-4d31-806d-047f8cd7885c.png"
+                alt="Logo"
+                className="mb-32"/>
+            <h1 className=" text-2xl font-bold mb-6 text-slate-800">Welcome to Alma!</h1>
+            <div className="mt-8 space-x-4">
                 <Link href="/lead_form">
-                    <button className="px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700 transition cursor-pointer">
-                        Lead Form
+                    <button className=" px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700
+                transition cursor-pointer">
+                        Submit Lead Form
                     </button>
                 </Link>
 
                 {auth.user ? (
                     <Link href="/leads">
-                        <button className="px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700 transition cursor-pointer">
-                            View Leads
+                        <button className=" px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bg-gray-700
+                transition cursor-pointer">
+                            Internal Leads (Admin)
                         </button>
                     </Link>
                 ) : (
                     <button
                         onClick={auth.login}
-                        className="px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bbg-gray-700 transition cursor-pointer">
-                        View Leads
+                        className=" px-6 py-3 bg-gray-800 text-white rounded-lg shadow-md hover:bbg-gray-700 transition
+                cursor-pointer">
+                        Internal Leads (Admin)
                     </button>
                 )}
             </div>
